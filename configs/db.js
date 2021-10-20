88e6e79731475b1fbdfacea603704ce34dc48ba1
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-20 10:06:36 +0545

    Change name of environment variable

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.prodMongoURI, {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
